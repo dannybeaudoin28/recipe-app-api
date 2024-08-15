733ca50fcf0fad8711b61a6f387b581bd23ba4e7
dannybeaudoin28 <danny.beaudoin@student.sl.on.ca>
2024-08-15 09:45:26 -0400

    Modify get or create tags

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -50,6 +50,7 @@ class RecipeSerializer(serializers.ModelSerializer):
 
         auth_user = self.context['request'].user
         for tag in tags:
+            print(tag)
             tag_obj, created = Tag.objects.get_or_create(
                 user=auth_user,
                 **tag,
